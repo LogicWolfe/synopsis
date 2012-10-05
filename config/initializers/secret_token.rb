5e0b1be0eec1d565b92eaac8935bdde6db5eb91c
Nathan Baxter <nathan.baxter@airbnb.com>
2012-10-05 04:44:36 -0700

    Generated Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Synopsis::Application.config.secret_token = '9f138e636933e3f0f9ac10e1a292155156ec7140759a64429cd3a485c145c5269ba91837bebd7744a791af824dd8c00a3937d4634c72976ca2caa93c73cc832a'
